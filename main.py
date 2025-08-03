4d2d1b2d2cb3b208e92fcbd60ee5014779849b2c
ilyesHamdiii <ilyesilyeshamdi10@gmail.com>
2025-08-03 16:59:27 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+import os 
+import sys
+load_dotenv()
+from google import genai
+from google.genai import types
+
+
+
+def main():
+    api_key=os.environ.get("GEMINI_API_KEY")
+    client=genai.Client(api_key=api_key)
+    if len(sys.argv)>1:
+        x=sys.argv[1]
+    else:
+        raise Exception("Error")
+        print("error")
+        sys.exit(1)
+    messages=[
+        types.Content(role="user",parts=[types.Part(text=x)])
+    ]
+
+    response=client.models._generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages
+    )
+    print(response.text)
+
+    if "--verbose" in sys.argv:
+        print("no verbose")
+        return 
+        print(f"User prompt : {x}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
